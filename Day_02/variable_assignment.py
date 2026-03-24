03b8bde6cab0281f9c14d9d677c3c9ea596c20bf
Parth Mohan <parth.mohan.india@gmail.com>
2026-03-24 20:45:47 +0800

    Doing Day 02 of the course!

---
+++ Day_02/variable_assignment.py
@@ -0,0 +1,19 @@
+# Day 2: 30 Days of Python Programming
+first_name = "Bob"
+last_name = 'Smith'
+full_name = "Bob Smith"
+country_name = 'India'
+city_name = "Mumbai"
+age_number = 30
+year_number = 2024
+is_married = False
+is_true = False
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country_name))
+
+
+print(len(first_name))
+print(len(last_name))
