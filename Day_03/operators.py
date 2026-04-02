36f5194d766524418fbdc967fb78c93237e1e28a
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-02 18:46:03 +0800

    Day_03 of the course

---
+++ Day_03/operators.py
@@ -0,0 +1,10 @@
+# Quick variable assignment exercise 
+
+
+current_age = 16 # Integer variable
+current_height = 1.75 # Float variable
+complex_number = 2 + 3j # Complex variable assignment
+
+
+
+
---
+++ Day_03/triangle_area.py
@@ -0,0 +1,6 @@
+triangle_base = int(input('Enter base:')) # Getting integer input from user 
+triangle_height = int(input('Enter height: ')) # Getting integer input from user regarding the triangle height
+
+area_of_triangle = (triangle_base * triangle_height) / 2 # Calculating the area of a triangle
+
+print(area_of_triangle) # Print the area of a triangle 
