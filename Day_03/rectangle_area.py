af6c595ca61ba683e8ac5ad0344d3ea17767b9ab
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-06 12:15:32 +0800

    Rectangle area / Triangle perimeter programs

---
+++ Day_03/rectangle_area.py
@@ -0,0 +1,7 @@
+
+length_of_rect = int(input('Enter the length of the recatangle: ')) # Gets the length of the rectangle through the int((input)) function
+width_of_rect = int(input('Enter the width of the rectangle: ')) # Gets the width of the rectangle through the int((input)) function
+
+rectangle_area = length_of_rect * width_of_rect # Calculates the area of the rectangle 
+
+print(f'The area of the rectangle is {rectangle_area}') # Prints the desired output 
---
+++ Day_03/triangle_perimeter.py
@@ -0,0 +1,7 @@
+side_a = int(input('Enter side a: '))
+side_b = int(input('Enter side b: '))
+side_c = int(input('Enter side c: '))
+
+perimeter = side_a + side_b + side_c
+
+print(f'The perimeter of this triangle is {perimeter}')
