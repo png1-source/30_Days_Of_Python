b9de7d04e1a509b229b621440b905c5f5b8efbdf
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-07 10:53:31 +0800

    Day_03 exercises

---
+++ Day_03/circle_radius.py
@@ -0,0 +1,10 @@
+import math
+
+radius_of_circle = int(input('Enter the radius of the circle: '))
+
+circle_area = math.pi * radius_of_circle * radius_of_circle # Calculates the area of the circle
+circle_circumference = 2 * math.pi * radius_of_circle # Caculates the circumference of the circle
+
+print(f'The area of your circle is {circle_area}') # Circle area output
+print(f' The circumference of your circle is {circle_circumference}') # Circle circumference output 
+
---
+++ Day_03/persons_age.py
@@ -0,0 +1,5 @@
+number_of_years_lived = int(input('Enter the number of hears you have lived: '))
+
+number_of_seconds_lived = number_of_years_lived * 31536000 
+
+print(f'You have lived for {number_of_seconds_lived} seconds')
