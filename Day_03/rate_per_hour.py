90ef1e82cb647bafc08a3e2a7b94583da2ca68f2
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-08 14:05:01 +0800

    Day_03 exercises

---
+++ Day_03/rate_per_hour.py
@@ -0,0 +1,7 @@
+hours = int(input('Enter the number of hours worked: '))
+rate = int(input('Enter the rate per hour: '))
+
+weekly_earning = hours * rate 
+
+print(f'Your weekly earning is {weekly_earning}')
+            
