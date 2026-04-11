4c891a6fd07f7aa7e26e855ff8b72465277ac8cb
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-11 20:03:25 +0800

    Day_04 exercises

---
+++ Day_04/combining_strings.py
@@ -0,0 +1,6 @@
+combined_string_01 = 'Thirty ' + 'Days ' + 'Of ' + 'Python'
+print(combined_string_01) # Combining individual strings to make a combined string and ensuring spaces to ensure spaces in the final total string
+
+combined_string_02 = 'Coding ' + 'For ' + 'All'
+print(combined_string_02) # Similar to the first oen
+
---
+++ Day_04/strings.py
@@ -0,0 +1,4 @@
+multiline_string = """ This is a 
+multiline string. It can span multiple lines.""" # By using triple """", this allows the printed string to be spread over multiple lines of code
+
+print(multiline_string)
