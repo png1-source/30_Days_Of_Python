454a96b62fb4f48f9ffec7fda8403977f98a29df
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-11 22:00:15 +0800

    Day_04 exercises

---
+++ Day_04/company_exericse.py
@@ -0,0 +1,12 @@
+company = "Coding for all"
+print(company) # Inital oupout of company 
+
+print(len(company)) # Outputting the length of the string comapny
+
+print(company.upper()) # Outputting the sting in all capital letters
+
+print(company.lower()) # Outputting the string in all lowercase letters
+
+
+
+
