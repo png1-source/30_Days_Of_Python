628b93a1dbd4839c7993f4011f847e28d33f1ef9
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-15 18:32:13 +0800

    Day_04 Exercises Completed

---
+++ Day_04/new_line.py
@@ -0,0 +1,4 @@
+print(""" I am enjoying this challenge.
+I just wonder what is next.""")
+
+# Prining a new line using \n
---
+++ Day_04/tab_esc_sequence.py
@@ -0,0 +1,2 @@
+print('Name\tAge\tCountry\tCity') # Printing the header of the table using \t to separate the columns
+print('Asabeneh\t250\tFinland\tHelsinki') # Printing the data of the table using \t to separate the columns
