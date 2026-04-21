c7244bf6496b485be4d61719da685e7e367aa868
Parth Mohan <parth.mohan.india@gmail.com>
2026-04-21 20:05:38 +0800

    Day_05 Exercises

---
+++ Day_05/lists.py
@@ -0,0 +1,3 @@
+cars = ["Toyota", "Honda", "Ford"] # Creates the list of cars 
+first_car = cars[0] # Assigns the first_car variable to the first item in the list 
+print(first_car) # Output: Toyota
